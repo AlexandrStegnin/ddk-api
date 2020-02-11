55e20f99ac0666572a67f9d131c3f3f282f84b10
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-11 19:14:44 +0500

    Обновление. Добавляем в константы сообщения.

--- src/main/java/com/ddkolesnik/ddkapi/util/Constants.java
+++ src/main/java/com/ddkolesnik/ddkapi/util/Constants.java
@@ -6,10 +6,14 @@ package com.ddkolesnik.ddkapi.util;
 
 public class Constants {
 
+    // PATHS
+
     public static final String API = "api";
 
     public static final String VERSION = "/v1";
 
     public static final String INVESTORS_PATH = API + VERSION + "/investors";
 
+    // MESSAGES
+    public static final String UNKNOWN_FACILITY = "Отсутствует название объекта";
 }
