34ab74022c4e1d9b0669b182b65e540be6b5fea8
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-11 19:03:28 +0500

    Добавили класс для хранения константных значений.

---
+++ src/main/java/com/ddkolesnik/ddkapi/util/Constants.java
@@ -0,0 +1,15 @@
+package com.ddkolesnik.ddkapi.util;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public class Constants {
+
+    public static final String API = "api";
+
+    public static final String VERSION = "/v1";
+
+    public static final String INVESTORS_PATH = API + VERSION + "/investors";
+
+}
