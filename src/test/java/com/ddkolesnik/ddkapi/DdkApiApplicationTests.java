263cb164655858d26afe84313d911f1268057d5a
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-11 15:46:05 +0500

    Инициализация проекта

---
+++ src/test/java/com/ddkolesnik/ddkapi/DdkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddkolesnik.ddkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
