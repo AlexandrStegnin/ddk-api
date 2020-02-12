bda25b92163bdb7d683cb5aceedfa1fd6b3a95e2
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-12 14:36:46 +0500

    Рефакторинг. Переименовали Constants в Constant. Добавили константные значения для путей.

---
+++ src/main/java/com/ddkolesnik/ddkapi/util/Constant.java
@@ -0,0 +1,24 @@
+package com.ddkolesnik.ddkapi.util;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public class Constant {
+
+    // PATHS
+
+    public static final String API = "api";
+
+    public static final String VERSION = "/v1";
+
+    public static final String PATH_INVESTORS = API + VERSION + "/investors";
+
+    public static final String PATH_MONIES = API + VERSION + "/monies";
+
+    // MESSAGES
+    public static final String UNKNOWN_FACILITY = "Неизвестный объект";
+
+    public static final String UNKNOWN_INVESTOR = "Неизместный инвестор";
+
+}
