79a14bfe8d8de33d16a07d8658986c1b4721a558
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-14 00:52:07 +0500

    Добавлена сущность Пользователь для использовании при авторизации.

---
+++ src/main/java/com/ddkolesnik/ddkapi/model/User.java
@@ -0,0 +1,71 @@
+package com.ddkolesnik.ddkapi.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@Entity
+@Table(name = "USERS", schema = "pss_projects", catalog = "pss_projects")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User implements UserDetails {
+
+    @Id
+    Long id;
+
+    String login;
+
+    String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinTable(
+            name = "USERS_ROLES",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"),
+            foreignKey = @ForeignKey(name = "user_role_to_user"),
+            inverseForeignKey = @ForeignKey(name = "user_role_to_role")
+    )
+    Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isAccountNonExpired() &&
+                isAccountNonLocked() &&
+                isCredentialsNonExpired();
+    }
+}
