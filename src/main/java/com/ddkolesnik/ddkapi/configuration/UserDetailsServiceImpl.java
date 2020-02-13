3c491eb33fdd45181017cc060f073e4005df4b92
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-14 00:56:31 +0500

    Реализован метод аутентификации пользователя.

---
+++ src/main/java/com/ddkolesnik/ddkapi/configuration/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ddkolesnik.ddkapi.configuration;
+
+import com.ddkolesnik.ddkapi.model.User;
+import com.ddkolesnik.ddkapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final User user = userRepository.findByLogin(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("No user found with login '%s'.", username));
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), user.isEnabled(), user.isAccountNonExpired(),
+                user.isCredentialsNonExpired(), user.isAccountNonLocked(), user.getRoles());
+    }
+
+}
