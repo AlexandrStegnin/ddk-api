49dcf5c7030eb076ab44c3ffa0e05f5cb8dafa9f
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-13 16:36:15 +0500

    Рефакторинг. Удалён неиспользуемый контроллер. Удалены неиспользуемые константы.

--- src/main/java/com/ddkolesnik/ddkapi/util/Constant.java
+++ src/main/java/com/ddkolesnik/ddkapi/util/Constant.java
@@ -8,17 +8,13 @@ public class Constant {
 
     // PATHS
 
-    public static final String API = "api";
-
-    public static final String VERSION = "/v1";
-
-    public static final String PATH_INVESTORS = VERSION + "/investors";
+    public static final String VERSION = "v1";
 
     public static final String PATH_MONIES = VERSION + "/monies";
 
     // MESSAGES
     public static final String UNKNOWN_FACILITY = "Неизвестный объект";
 
-    public static final String UNKNOWN_INVESTOR = "Неизместный инвестор";
+    public static final String UNKNOWN_INVESTOR = "Неизвестный инвестор";
 
 }
