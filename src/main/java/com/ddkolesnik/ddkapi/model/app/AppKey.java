68e9437e8588418b51a51b55d36f9a9061edaed7
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-11 16:55:09 +0500

    Добавляем сущность для получения информации о наличии ключа для доступа к приложению.

---
+++ src/main/java/com/ddkolesnik/ddkapi/model/app/AppKey.java
@@ -0,0 +1,47 @@
+package com.ddkolesnik.ddkapi.model.app;
+
+import com.ddkolesnik.ddkapi.model.AbstractEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+/**
+ * Модель для хранения информации о ключах приложения
+ * Ключ нужен для авторизации в API
+ *
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@Entity
+@Table(name = "app_key")
+@EqualsAndHashCode(callSuper = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AppKey extends AbstractEntity {
+
+    @Column(name = "app_name")
+    String name;
+
+    @Column(name = "app_key")
+    String key;
+
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(name = "creation_time")
+    LocalDate creationTime;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    @Column(name = "modified_time")
+    LocalDate modifiedTime;
+
+}
