5efc2e31f50405e45ef70f376ed6ab7bbf5be81a
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-11 21:02:56 +0500

    Добавили класс для вывода читаемого вида ошибок.

---
+++ src/main/java/com/ddkolesnik/ddkapi/configuration/ApiErrorResponse.java
@@ -0,0 +1,28 @@
+package com.ddkolesnik.ddkapi.configuration;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+
+import java.time.Instant;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Getter
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class ApiErrorResponse {
+
+    HttpStatus status;
+    String message;
+    Instant timestamp;
+
+    public ApiErrorResponse(HttpStatus status, String message, Instant timestamp) {
+        this.status= status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+}
