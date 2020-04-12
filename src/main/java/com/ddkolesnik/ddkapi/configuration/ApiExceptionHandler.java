069f4aeee7a008d6fc32d701af37fa8fd146141d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-12 13:47:34 +0500

    Добавили читаемый вывод ошибок валидации.

--- src/main/java/com/ddkolesnik/ddkapi/configuration/ApiExceptionHandler.java
+++ src/main/java/com/ddkolesnik/ddkapi/configuration/ApiExceptionHandler.java
@@ -1,11 +1,19 @@
 package com.ddkolesnik.ddkapi.configuration;
 
 import com.ddkolesnik.ddkapi.exception.ApiException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author Alexandr Stegnin
@@ -17,6 +25,29 @@ public class ApiExceptionHandler {
     @ExceptionHandler({ ApiException.class })
     protected ResponseEntity<ApiErrorResponse> handleApiException(ApiException ex) {
         return new ResponseEntity<>(new ApiErrorResponse(ex.getStatus(), ex.getMessage(), Instant.now()), ex.getStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ApiErrorResponse> handle(ConstraintViolationException exception) {
+        String errorMessage = new ArrayList<>(exception.getConstraintViolations())
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining( ",\n"));
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(HttpStatus.BAD_REQUEST, errorMessage, Instant.now());
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ApiErrorResponse> handle(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(FieldError::getField, Objects.requireNonNull(FieldError::getDefaultMessage)))
+                .entrySet()
+                .stream()
+                .map(entrySet -> entrySet.getKey() + ": "+ entrySet.getValue()).
+                collect(Collectors.joining(",\n"));
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(HttpStatus.BAD_REQUEST, message, Instant.now());
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
