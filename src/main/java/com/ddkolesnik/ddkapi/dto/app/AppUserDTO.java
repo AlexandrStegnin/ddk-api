a8ddd96ad18a1ab8d29caa55b9ffc5b0c47b404d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-13 18:21:50 +0500

    Добавлена сущность и DTO для создания пользователей.

---
+++ src/main/java/com/ddkolesnik/ddkapi/dto/app/AppUserDTO.java
@@ -0,0 +1,33 @@
+package com.ddkolesnik.ddkapi.dto.app;
+
+import com.ddkolesnik.ddkapi.model.app.AppUser;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AppUserDTO {
+
+    @NotNull(message = "Код партнёра должен быть указан")
+    String login;
+
+    String lastName;
+
+    String email;
+
+    public AppUserDTO(AppUser entity) {
+        this.login = entity.getLogin();
+        this.lastName = entity.getLastName();
+        this.email = entity.getEmail();
+    }
+
+}
---
+++ src/main/java/com/ddkolesnik/ddkapi/model/app/AppUser.java
@@ -0,0 +1,53 @@
+package com.ddkolesnik.ddkapi.model.app;
+
+import com.ddkolesnik.ddkapi.model.Role;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@Entity
+@Table(name = "USERS")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "login")
+    String login;
+
+    @Column(name = "password")
+    String password;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "email")
+    String email;
+
+    @ManyToMany
+    @JoinTable(name = "USERS_ROLES",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<Role> roles = new HashSet<>();
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+
+    @PrePersist
+    public void setLogin() {
+        this.login = "investor".concat(this.login);
+    }
+
+}
