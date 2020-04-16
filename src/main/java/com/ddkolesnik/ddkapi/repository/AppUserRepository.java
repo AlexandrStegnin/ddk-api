dd379d30fb14cd6b1ba6b07088f77390858171f6
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-16 15:40:44 +0500

    Добавили метод получения пользователя по роли.

--- src/main/java/com/ddkolesnik/ddkapi/repository/AppUserRepository.java
+++ src/main/java/com/ddkolesnik/ddkapi/repository/AppUserRepository.java
@@ -1,8 +1,12 @@
 package com.ddkolesnik.ddkapi.repository;
 
+import com.ddkolesnik.ddkapi.model.Role;
 import com.ddkolesnik.ddkapi.model.app.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Alexandr Stegnin
@@ -12,5 +16,7 @@ import org.springframework.stereotype.Repository;
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     AppUser findByLogin(String login);
+
+    List<AppUser> findByRolesIn(Set<Role> roles);
 
 }
