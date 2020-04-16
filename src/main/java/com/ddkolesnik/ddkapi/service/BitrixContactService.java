09eb2a8ca31b908cefaff6d484f3f68df4e87d16
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-16 16:08:06 +0500

    Добавили метод для синхронизации данных Битрикс 24 и ddkolesnik.com

--- src/main/java/com/ddkolesnik/ddkapi/service/BitrixContactService.java
+++ src/main/java/com/ddkolesnik/ddkapi/service/BitrixContactService.java
@@ -1,23 +1,32 @@
 package com.ddkolesnik.ddkapi.service;
 
+import com.ddkolesnik.ddkapi.dto.app.AppUserDTO;
 import com.ddkolesnik.ddkapi.dto.bitrix.BitrixContactDTO;
+import com.ddkolesnik.ddkapi.dto.bitrix.BitrixEmailDTO;
 import com.ddkolesnik.ddkapi.model.bitrix.BitrixContact;
 import com.ddkolesnik.ddkapi.repository.BitrixContactRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
  * @author Alexandr Stegnin
  */
 
+@Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class BitrixContactService {
@@ -25,6 +34,8 @@ public class BitrixContactService {
     BitrixContactRepository contactRepository;
 
     ModelMapper mapper;
+
+    AppUserService userService;
 
     public BitrixContact findById(String id) {
         return contactRepository.findById(id)
@@ -39,10 +50,46 @@ public class BitrixContactService {
     }
 
     public List<BitrixContactDTO> findAll() {
-        return contactRepository.findAll()
+        List<BitrixContact> contacts = contactRepository.findAll();
+        return contacts
                 .stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
+    }
+
+    public void mergeContacts() {
+        List<BitrixContactDTO> contacts = findAll();
+        List<AppUserDTO> users = userService.getAllDTO();
+        List<AppUserDTO> updatedUsers = new ArrayList<>();
+        contacts.forEach(contact -> users.stream()
+                .filter(user -> Objects.nonNull(user.getPartnerCode()) &&
+                        user.getPartnerCode().endsWith(contact.getCode()))
+                .forEach(user -> {
+                    if (contact.getEmails().size() == 1) {
+                        contact.getEmails().stream().findFirst().ifPresent(email -> {
+                            if (!user.getEmail().equalsIgnoreCase(email.getEmail())) {
+                                user.setEmail(email.getEmail());
+                                user.setPartnerCode(contact.getCode());
+                                if (!updatedUsers.contains(user)) {
+                                    updatedUsers.add(user);
+                                }
+                            }
+                        });
+                    } else if (contact.getEmails().size() > 0) {
+                        contact.getEmails().stream()
+                                .sorted(Comparator.comparing(BitrixEmailDTO::getType))
+                                .forEach(email -> {
+                                    if (!user.getEmail().equalsIgnoreCase(email.getEmail())) {
+                                        user.setEmail(email.getEmail());
+                                        user.setPartnerCode(contact.getCode());
+                                        if (!updatedUsers.contains(user)) {
+                                            updatedUsers.add(user);
+                                        }
+                                    }
+                                });
+                    }
+                }));
+        updatedUsers.forEach(userService::update);
     }
 
     private BitrixContactDTO convertToDTO(BitrixContact contact) {
