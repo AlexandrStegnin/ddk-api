90e056ecfb0cfb91accf2809c7779d46ccfef94e
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-06-30 10:22:30 +0500

    Добавлен вид денег.

---
+++ src/main/java/com/ddkolesnik/ddkapi/model/log/CashType.java
@@ -0,0 +1,43 @@
+package com.ddkolesnik.ddkapi.model.log;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public enum CashType {
+
+    NEW(1, "Новая сумма"),
+    OLD(2, "Старая сумма"),
+    UNDEFINED(0, "Не определено"),
+    INVESTOR_CASH(3, "Деньги инвесторов"),
+    SALE_CASH(4, "Деньги с продажи"),
+    RENT_CASH(5, "Деньги с аренды"),
+    CASH_1C(6, "Проводка из 1С");
+
+    private final int id;
+
+    private final String title;
+
+    CashType(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static CashType fromTitle(String title) {
+        for (CashType value : values()) {
+            if (value.title.equalsIgnoreCase(title)) {
+                return value;
+            }
+        }
+        return UNDEFINED;
+    }
+
+}
