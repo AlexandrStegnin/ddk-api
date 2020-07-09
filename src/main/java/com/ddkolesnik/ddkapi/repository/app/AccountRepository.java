e168890e678a2e96ccfed330a35555e235a3cd50
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-07-09 23:51:03 +0500

    Добавлены и реализованы методы для работы со счетами.

---
+++ src/main/java/com/ddkolesnik/ddkapi/repository/app/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.ddkolesnik.ddkapi.repository.app;
+
+import com.ddkolesnik.ddkapi.model.app.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
---
+++ src/main/java/com/ddkolesnik/ddkapi/service/app/AccountService.java
@@ -0,0 +1,51 @@
+package com.ddkolesnik.ddkapi.service.app;
+
+import com.ddkolesnik.ddkapi.model.app.Account;
+import com.ddkolesnik.ddkapi.model.app.AppUser;
+import com.ddkolesnik.ddkapi.repository.app.AccountRepository;
+import com.ddkolesnik.ddkapi.util.Constant;
+import com.ddkolesnik.ddkapi.util.Region;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Service
+@Transactional
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class AccountService {
+
+    AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public void createAccount(AppUser user) {
+        Account account = new Account();
+        account.setAccountNumber(generateAccountNumber(user));
+        account.setOwnerId(user.getId());
+        accountRepository.save(account);
+    }
+
+    private String generateAccountNumber(AppUser user) {
+        /*
+        первые 5 цифр 00000 (порядковый номер клиента),
+        вторые 3 цифры (номер региона),
+        далее 4 цифры (порядковый номер объекта),
+        далее 2 цифры (порядковый номер подобъекта) - всего 14 символов поллучается
+         */
+        String clientCode = user.getLogin().substring(Constant.INVESTOR_PREFIX.length());
+        String regionNumber = getRegionNumber();
+        return clientCode.concat(regionNumber);
+    }
+
+    private String getRegionNumber() {
+        return Region.TMN.getNumber();
+    }
+
+}
