24e6e81c693dd26f28a3b8e555c2960bfad92000
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-07-09 23:50:27 +0500

    Добавлена константа

--- src/main/java/com/ddkolesnik/ddkapi/util/Constant.java
+++ src/main/java/com/ddkolesnik/ddkapi/util/Constant.java
@@ -79,4 +79,5 @@ public class Constant {
 
     public static final String[] ALL_SWAGGER_MATCHERS = {"/v3/api-docs*", "/configuration/**", "/swagger*/**", "/webjars/**", "/", "/info", "/api-info.html"};
 
+    public static final String INVESTOR_PREFIX = "investor";
 }
