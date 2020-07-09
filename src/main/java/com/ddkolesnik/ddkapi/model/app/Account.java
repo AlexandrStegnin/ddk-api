efebdc09d9d49353cb569d905de608df96041baa
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-07-09 23:50:44 +0500

    Добавлена сущность счёт

---
+++ src/main/java/com/ddkolesnik/ddkapi/model/app/Account.java
@@ -0,0 +1,34 @@
+package com.ddkolesnik.ddkapi.model.app;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@Entity
+@Table(name = "account")
+@ToString(of = {"id", "accountNumber"})
+@EqualsAndHashCode(of = {"id", "accountNumber"})
+public class Account {
+
+    @Id
+    @TableGenerator(name = "accountSeqStore", table = "SEQ_STORE",
+            pkColumnName = "SEQ_NAME", pkColumnValue = "ACCOUNT.ID.PK",
+            valueColumnName = "SEQ_VALUE", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "accountSeqStore")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    @Column(name = "owner_id")
+    private Long ownerId;
+
+}
