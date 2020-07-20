e5483e49d2b1ca11d19ff6c786ff0f53259244e0
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-07-20 22:25:17 +0500

    Обновление. Добавлен поиск подобъекта с суффиксом "_Целиком". Добавление в деньги "Вида доли". Мелкий рефакторинг.

--- src/main/java/com/ddkolesnik/ddkapi/service/cash/InvestorCashService.java
+++ src/main/java/com/ddkolesnik/ddkapi/service/cash/InvestorCashService.java
@@ -5,11 +5,15 @@ import com.ddkolesnik.ddkapi.model.cash.CashSource;
 import com.ddkolesnik.ddkapi.model.money.Facility;
 import com.ddkolesnik.ddkapi.model.money.Investor;
 import com.ddkolesnik.ddkapi.model.money.Money;
+import com.ddkolesnik.ddkapi.model.money.UnderFacility;
 import com.ddkolesnik.ddkapi.repository.money.MoneyRepository;
 import com.ddkolesnik.ddkapi.service.SendMessageService;
 import com.ddkolesnik.ddkapi.service.log.TransactionLogService;
 import com.ddkolesnik.ddkapi.service.money.FacilityService;
 import com.ddkolesnik.ddkapi.service.money.InvestorService;
+import com.ddkolesnik.ddkapi.service.money.UnderFacilityService;
+import com.ddkolesnik.ddkapi.util.Constant;
+import com.ddkolesnik.ddkapi.util.ShareType;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -29,8 +33,6 @@ import org.springframework.transaction.annotation.Transactional;
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class InvestorCashService {
 
-    private static final String INVESTOR_PREFIX = "investor";
-
     MoneyRepository moneyRepository;
 
     InvestorService investorService;
@@ -42,6 +44,8 @@ public class InvestorCashService {
     SendMessageService messageService;
 
     TransactionLogService transactionLogService;
+
+    UnderFacilityService underFacilityService;
 
     /**
      * Создать или обновить проводку, пришедшую из 1С
@@ -123,6 +127,10 @@ public class InvestorCashService {
 
         Facility facility = findFacility(dto.getFacility());
         money.setFacility(facility);
+        UnderFacility underFacility = findUnderFacility(facility);
+        money.setUnderFacility(underFacility);
+
+        money.setShareType(ShareType.MAIN);
 
         CashSource cashSource = findCashSource(dto.getCashSource());
         money.setCashSource(cashSource);
@@ -140,7 +148,7 @@ public class InvestorCashService {
      * @return - найденный инвестор
      */
     private Investor findInvestor(String investorCode) {
-        String login = INVESTOR_PREFIX.concat(investorCode);
+        String login = Constant.INVESTOR_PREFIX.concat(investorCode);
         return investorService.findByLogin(login);
     }
 
@@ -148,10 +156,24 @@ public class InvestorCashService {
      * Найти объект по имени, который приходит из 1С
      *
      * @param fullName - название объекта
-     * @return - найденный инвестор
+     * @return - найденный объект
      */
     private Facility findFacility(String fullName) {
         return facilityService.findByFullName(fullName);
+    }
+
+    /**
+     * Найти подобъект по имени с суффиксом "_Целиком"
+     *
+     * @param facility - объект
+     * @return - найденный подобъект
+     */
+    private UnderFacility findUnderFacility(Facility facility) {
+        if (facility != null) {
+            String fullName = facility.getName().concat(Constant.UNDER_FACILITY_SUFFIX);
+            return underFacilityService.findByName(fullName);
+        }
+        return null;
     }
 
     /**
