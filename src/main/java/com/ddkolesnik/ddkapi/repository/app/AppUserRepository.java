fae27a570419749596a3aa9547279fe1aa1e5b2d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-08 23:40:10 +0500

    Попытка исправить формирование не верного запроса JPA к MySQL.

--- src/main/java/com/ddkolesnik/ddkapi/repository/app/AppUserRepository.java
+++ src/main/java/com/ddkolesnik/ddkapi/repository/app/AppUserRepository.java
@@ -3,6 +3,8 @@ package com.ddkolesnik.ddkapi.repository.app;
 import com.ddkolesnik.ddkapi.model.security.Role;
 import com.ddkolesnik.ddkapi.model.app.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,7 +17,8 @@ import java.util.Set;
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
-    AppUser findByLogin(String login);
+    @Query("SELECT au FROM AppUser au WHERE au.login = :login")
+    AppUser findByLogin(@Param("login") String login);
 
     List<AppUser> findByRolesIn(Set<Role> roles);
 
