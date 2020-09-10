104f26c5b797f7180468b4c8531eb9755b1d8238
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-10 15:36:21 +0500

    Добавлен метод поиска суммы по пришедшим параметрам из 1С.

--- src/main/java/com/ddkolesnik/ddkapi/repository/money/MoneyRepository.java
+++ src/main/java/com/ddkolesnik/ddkapi/repository/money/MoneyRepository.java
@@ -3,7 +3,12 @@ package com.ddkolesnik.ddkapi.repository.money;
 import com.ddkolesnik.ddkapi.model.money.Money;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 /**
  * @author Alexandr Stegnin
@@ -17,4 +22,11 @@ public interface MoneyRepository extends JpaRepository<Money, Long>, JpaSpecific
     Long countByInvestorIdAndDateClosingIsNull(Long investorId);
 
     void deleteByTransactionUUID(String transactionUUID);
+
+    @Query("SELECT m FROM Money m WHERE m.dateGiven = :dateGiven AND m.givenCash = :givenCash AND " +
+            "m.facility.fullName = :facilityName AND m.cashSource.organization = :organizationId AND m.investor.login = :login")
+    Money findMoney(@Param("dateGiven") LocalDate dateGiven, @Param("givenCash") BigDecimal givenCash,
+                    @Param("facilityName") String facilityName, @Param("organizationId") String organizationId,
+                    @Param("login") String login);
+
 }
