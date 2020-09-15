1a54b85947a4586ebc0575920235b6d7230b63d5
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-15 23:02:13 +0500

    Обновлён DTO для создания/обновления объектов из 1С.

--- src/main/java/com/ddkolesnik/ddkapi/dto/money/FacilityDTO.java
+++ src/main/java/com/ddkolesnik/ddkapi/dto/money/FacilityDTO.java
@@ -5,16 +5,28 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author Alexandr Stegnin
  */
 
 @Data
-@Schema(name = "Facility", implementation = FacilityDTO.class, description = "Информация об объекте")
+@Schema(name = "Facility", description = "Информация об объекте")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class FacilityDTO {
 
-    @Schema(implementation = String.class, name = "Name", description = "Название объекта")
+    @NotBlank(message = "Название должно быть указано")
+    @Schema(implementation = String.class, name = "name", description = "Краткое название объекта", required = true)
     String name;
+
+    @Schema(implementation = String.class, name = "city", description = "Город объекта")
+    String city;
+
+    @Schema(implementation = String.class, name = "fullName", description = "Полное название объекта")
+    String fullName;
+
+    @Schema(implementation = String.class, name = "projectUUID", description = "Идентификатор объекта из 1С")
+    String projectUUID;
 
 }
