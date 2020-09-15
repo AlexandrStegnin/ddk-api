f62559eb31adc8671f94f673ac08e45e7e728472
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-15 23:58:18 +0500

    Добавлен метод проверки наличия в системе аналогичного номера счёта.

--- src/main/java/com/ddkolesnik/ddkapi/repository/app/AccountRepository.java
+++ src/main/java/com/ddkolesnik/ddkapi/repository/app/AccountRepository.java
@@ -10,4 +10,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+
 }
