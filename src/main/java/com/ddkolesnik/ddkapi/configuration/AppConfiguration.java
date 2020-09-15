f2b21f5d1ffff35354d3ab93e9609cfef135078a
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-15 23:00:33 +0500

    Добавлен bean для отлова RequestRejectedException.

--- src/main/java/com/ddkolesnik/ddkapi/configuration/AppConfiguration.java
+++ src/main/java/com/ddkolesnik/ddkapi/configuration/AppConfiguration.java
@@ -1,10 +1,13 @@
 package com.ddkolesnik.ddkapi.configuration;
 
+import com.ddkolesnik.ddkapi.configuration.exception.LogFilter;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.RegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -37,5 +40,15 @@ public class AppConfiguration {
     @Bean(name = "threadPoolTaskExecutor")
     public Executor threadPoolTaskExecutor() {
         return new ThreadPoolTaskExecutor();
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Bean
+    public FilterRegistrationBean someFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new LogFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(RegistrationBean.HIGHEST_PRECEDENCE); //Must has higher precedence than FilterChainProxy
+        return registration;
     }
 }
