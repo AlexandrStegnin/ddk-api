544f058b3314fb101e5c7577b4206182d16fb4c2
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-15 23:04:16 +0500

    Добавлены методы создания/обновления объекта на основе данных из 1С.

--- src/main/java/com/ddkolesnik/ddkapi/service/money/FacilityService.java
+++ src/main/java/com/ddkolesnik/ddkapi/service/money/FacilityService.java
@@ -1,6 +1,7 @@
 package com.ddkolesnik.ddkapi.service.money;
 
 import com.ddkolesnik.ddkapi.configuration.exception.ApiException;
+import com.ddkolesnik.ddkapi.dto.money.FacilityDTO;
 import com.ddkolesnik.ddkapi.model.money.Facility;
 import com.ddkolesnik.ddkapi.repository.money.FacilityRepository;
 import lombok.AccessLevel;
@@ -28,6 +29,63 @@ public class FacilityService {
             throw new ApiException("Объект с названием = [" + fullName + "] не найден", HttpStatus.NOT_FOUND);
         }
         return facility;
+    }
+
+    /**
+     * Обновить или создать объект на основе DTO из 1С
+     *
+     * @param dto DTO объекта из 1С
+     */
+    public void update(FacilityDTO dto) {
+        if (dto.getProjectUUID() == null || dto.getProjectUUID().isEmpty()) {
+            throw new ApiException("Не задан идентификатор объекта", HttpStatus.BAD_REQUEST);
+        }
+        Facility facility = findByProjectUUID(dto.getProjectUUID());
+        if (facility == null) {
+            facility = facilityRepository.findByFullNameEqualsIgnoreCase(dto.getFullName());
+            if (facility != null) {
+                updateFacility(facility, dto);
+            }
+        }
+        if (facility == null) {
+            createFacility(dto);
+        }
+    }
+
+    /**
+     * Обновить объект на основе DTO из 1С
+     *
+     * @param facility объект для обновления
+     * @param dto DTO объект из 1С
+     */
+    private void updateFacility(Facility facility, FacilityDTO dto) {
+        facility.setProjectUUID(dto.getProjectUUID());
+        facility.setName(dto.getName());
+        facility.setFullName(dto.getName());
+        facilityRepository.save(facility);
+    }
+
+    /**
+     * Создать объект на основе DTO из 1С
+     *
+     * @param dto DTO из 1С
+     */
+    private void createFacility(FacilityDTO dto) {
+        Facility facility = new Facility();
+        facility.setName(dto.getName());
+        facility.setFullName(dto.getName());
+        facility.setProjectUUID(dto.getProjectUUID());
+        facilityRepository.save(facility);
+    }
+
+    /**
+     * Найти объект по идентификатору из 1С
+     *
+     * @param projectUUID идентификатор из 1С
+     * @return найденный объект
+     */
+    public Facility findByProjectUUID(String projectUUID) {
+        return facilityRepository.findByProjectUUID(projectUUID);
     }
 
 }
