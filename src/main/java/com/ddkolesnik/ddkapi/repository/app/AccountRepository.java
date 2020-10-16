a508f566ec26c7e95d851bcdcb35866413d47ae2
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-10-16 19:01:02 +0500

    Добавлен метод поиска счёта по виду и id владельца

--- src/main/java/com/ddkolesnik/ddkapi/repository/app/AccountRepository.java
+++ src/main/java/com/ddkolesnik/ddkapi/repository/app/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.ddkolesnik.ddkapi.repository.app;
 
 import com.ddkolesnik.ddkapi.model.app.Account;
+import com.ddkolesnik.ddkapi.util.OwnerType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +13,7 @@ import org.springframework.stereotype.Repository;
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     boolean existsByAccountNumber(String accountNumber);
+
+    Account findByOwnerIdAndOwnerType(Long ownerId, OwnerType ownerType);
 
 }
