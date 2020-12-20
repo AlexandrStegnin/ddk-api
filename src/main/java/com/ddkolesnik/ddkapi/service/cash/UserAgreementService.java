bc54f16cb7dc802a4d2be0f1db6b803ae0458f55
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-12-20 15:57:59 +0500

    Добавлен метод создания записи на основе данных из Битрикс 24.

--- src/main/java/com/ddkolesnik/ddkapi/service/cash/UserAgreementService.java
+++ src/main/java/com/ddkolesnik/ddkapi/service/cash/UserAgreementService.java
@@ -1,7 +1,12 @@
 package com.ddkolesnik.ddkapi.service.cash;
 
+import com.ddkolesnik.ddkapi.dto.cash.UserAgreementDTO;
+import com.ddkolesnik.ddkapi.model.app.AppUser;
 import com.ddkolesnik.ddkapi.model.cash.UserAgreement;
+import com.ddkolesnik.ddkapi.model.money.Facility;
 import com.ddkolesnik.ddkapi.repository.cash.UserAgreementRepository;
+import com.ddkolesnik.ddkapi.service.app.AppUserService;
+import com.ddkolesnik.ddkapi.service.money.FacilityService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -20,6 +25,10 @@ public class UserAgreementService {
 
     UserAgreementRepository userAgreementRepository;
 
+    FacilityService facilityService;
+
+    AppUserService appUserService;
+
     public UserAgreement findById(Long id) {
         return userAgreementRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Информация о заключённом договоре не найдена"));
@@ -35,6 +44,23 @@ public class UserAgreementService {
 
     public void delete(UserAgreement userAgreement) {
         userAgreementRepository.delete(userAgreement);
+    }
+
+    /**
+     * Создать запись о том, с кем заключён договор на основе DTO
+     *
+     * @param dto DTO записи из Битрикс 24
+     * @return созданная запись
+     */
+    public UserAgreement create(UserAgreementDTO dto) {
+        Facility facility = facilityService.findByFullName(dto.getFacility());
+        AppUser investor = appUserService.findByLogin(dto.getConcludedWith());
+        UserAgreement userAgreement = new UserAgreement();
+        userAgreement.setFacilityId(facility.getId());
+        userAgreement.setConcludedWith(investor.getId());
+        userAgreement.setTaxRate(dto.getTaxRate());
+        userAgreement.setConcludedFrom(dto.getConcludedFrom());
+        return userAgreementRepository.save(userAgreement);
     }
 
 }
