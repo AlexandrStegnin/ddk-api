e3470b95eda990c27771fbae85e0faac120e4556
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-12-23 11:41:55 +0500

    Рефакторинг. Генерируем id автоматически без использования таблицы

--- src/main/java/com/ddkolesnik/ddkapi/model/log/InvestorCashLog.java
+++ src/main/java/com/ddkolesnik/ddkapi/model/log/InvestorCashLog.java
@@ -22,10 +22,7 @@ import java.time.LocalDate;
 public class InvestorCashLog {
 
     @Id
-    @TableGenerator(name = "invCashLogSeqStore", table = "SEQ_STORE",
-            pkColumnName = "SEQ_NAME", pkColumnValue = "INV.CASH.LOG.ID.PK",
-            valueColumnName = "SEQ_VALUE", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "invCashLogSeqStore")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
 
