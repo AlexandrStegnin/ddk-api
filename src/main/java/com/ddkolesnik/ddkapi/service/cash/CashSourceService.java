fd9366a7994a85b48e9dff0fc2695c9ee83b2cf0
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-01-19 09:06:06 +0500

    Добавлено логирование.

--- src/main/java/com/ddkolesnik/ddkapi/service/cash/CashSourceService.java
+++ src/main/java/com/ddkolesnik/ddkapi/service/cash/CashSourceService.java
@@ -6,15 +6,15 @@ import com.ddkolesnik.ddkapi.repository.cash.CashSourceRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.Objects;
 
 /**
  * @author Alexandr Stegnin
  */
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -24,8 +24,10 @@ public class CashSourceService {
 
     public CashSource findByOrganization(String organization) {
         CashSource cashSource = cashSourceRepository.findByOrganization(organization);
-        if (Objects.isNull(cashSource)) {
-            throw new ApiException("Источник денег [" + organization + "] не найден.", HttpStatus.NOT_FOUND);
+        if (cashSource == null) {
+            String message = String.format("Источник денег [%s] не найден.", organization);
+            log.error(message);
+            throw new ApiException(message, HttpStatus.NOT_FOUND);
         }
         return cashSource;
     }
