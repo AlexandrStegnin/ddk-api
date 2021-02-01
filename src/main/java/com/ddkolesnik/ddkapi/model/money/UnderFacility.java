4b0a46dc35c38836223f274a6ca9fa407c64f3c1
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-02-01 14:32:17 +0500

    Исправление. Добавлены геттеры/сеттеры/equals/hashcode

--- src/main/java/com/ddkolesnik/ddkapi/model/money/UnderFacility.java
+++ src/main/java/com/ddkolesnik/ddkapi/model/money/UnderFacility.java
@@ -1,6 +1,8 @@
 package com.ddkolesnik.ddkapi.model.money;
 
 import com.ddkolesnik.ddkapi.model.AbstractEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,8 +12,10 @@ import javax.persistence.Table;
  * @author Alexandr Stegnin
  */
 
+@Data
 @Entity
 @Table(name = "under_facility")
+@EqualsAndHashCode(callSuper = true)
 public class UnderFacility extends AbstractEntity {
 
     @Column(name = "name")
