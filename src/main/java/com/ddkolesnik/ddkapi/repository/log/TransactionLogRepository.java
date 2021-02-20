97af07289c5caf8c1ca5da08f9f28ba8994bf70f
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-02-20 11:07:53 +0500

    Добавлен метод поиска лога по транзакции по счёту.

--- src/main/java/com/ddkolesnik/ddkapi/repository/log/TransactionLogRepository.java
+++ src/main/java/com/ddkolesnik/ddkapi/repository/log/TransactionLogRepository.java
@@ -1,6 +1,7 @@
 package com.ddkolesnik.ddkapi.repository.log;
 
 import com.ddkolesnik.ddkapi.model.log.TransactionLog;
+import com.ddkolesnik.ddkapi.model.money.AccountTransaction;
 import com.ddkolesnik.ddkapi.model.money.Money;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -17,5 +18,7 @@ public interface TransactionLogRepository extends JpaRepository<TransactionLog, 
     List<TransactionLog> findByInvestorsCashesContains(Money cash);
 
     List<TransactionLog> findByBlockedFromId(Long blockedFromId);
+
+    List<TransactionLog> findByAccountTransactionsContains(AccountTransaction accountTransaction);
 
 }
