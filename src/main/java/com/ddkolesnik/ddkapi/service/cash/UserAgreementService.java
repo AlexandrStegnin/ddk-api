9dc6dd18653e20c8c4620f545a58217c763b0c7f
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-04-19 14:45:44 +0500

    Рефакторинг. Удалён неиспользуемый код.

--- src/main/java/com/ddkolesnik/ddkapi/service/cash/UserAgreementService.java
+++ src/main/java/com/ddkolesnik/ddkapi/service/cash/UserAgreementService.java
@@ -15,8 +15,6 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
-
 import static com.ddkolesnik.ddkapi.util.Constant.INVESTOR_PREFIX;
 
 /**
@@ -34,21 +32,8 @@ public class UserAgreementService {
 
     AppUserService appUserService;
 
-    public UserAgreement findById(Long id) {
-        return userAgreementRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Информация о заключённом договоре не найдена"));
-    }
-
-    public UserAgreement create(UserAgreement userAgreement) {
-        return userAgreementRepository.save(userAgreement);
-    }
-
     public UserAgreement update(UserAgreement userAgreement) {
         return userAgreementRepository.save(userAgreement);
-    }
-
-    public void delete(UserAgreement userAgreement) {
-        userAgreementRepository.delete(userAgreement);
     }
 
     /**
