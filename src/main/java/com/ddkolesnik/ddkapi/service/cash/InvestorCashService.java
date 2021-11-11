956aae1ed5d11c49b5b365c5a8a99c9fd78dfea7
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-11-11 17:35:44 +0500

    Разделение на несколько сервисов для удобства поддержки

--- src/main/java/com/ddkolesnik/ddkapi/service/cash/InvestorCashService.java
+++ src/main/java/com/ddkolesnik/ddkapi/service/cash/InvestorCashService.java
@@ -1,56 +1,51 @@
 package com.ddkolesnik.ddkapi.service.cash;
 
-import com.ddkolesnik.ddkapi.configuration.exception.ApiException;
 import com.ddkolesnik.ddkapi.configuration.exception.ApiSuccessResponse;
 import com.ddkolesnik.ddkapi.dto.cash.InvestorCashDTO;
-import com.ddkolesnik.ddkapi.model.app.Account;
-import com.ddkolesnik.ddkapi.model.app.AppUser;
 import com.ddkolesnik.ddkapi.model.cash.CashSource;
-import com.ddkolesnik.ddkapi.model.log.CashType;
 import com.ddkolesnik.ddkapi.model.log.TransactionLog;
 import com.ddkolesnik.ddkapi.model.money.*;
-import com.ddkolesnik.ddkapi.repository.app.AccountRepository;
-import com.ddkolesnik.ddkapi.repository.app.AppUserRepository;
 import com.ddkolesnik.ddkapi.repository.money.MoneyRepository;
+import com.ddkolesnik.ddkapi.service.CashingService;
+import com.ddkolesnik.ddkapi.service.ResaleShareService;
 import com.ddkolesnik.ddkapi.service.SendMessageService;
 import com.ddkolesnik.ddkapi.service.log.TransactionLogService;
+import com.ddkolesnik.ddkapi.service.mapper.MoneyMapper;
 import com.ddkolesnik.ddkapi.service.money.AccountTransactionService;
 import com.ddkolesnik.ddkapi.service.money.FacilityService;
 import com.ddkolesnik.ddkapi.service.money.InvestorService;
 import com.ddkolesnik.ddkapi.service.money.UnderFacilityService;
-import com.ddkolesnik.ddkapi.util.*;
+import com.ddkolesnik.ddkapi.util.AccountingCode;
+import com.ddkolesnik.ddkapi.util.Constant;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.Nullable;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static com.ddkolesnik.ddkapi.util.Constant.COMMISSION_RATE;
-import static com.ddkolesnik.ddkapi.util.Constant.INVESTOR_PREFIX;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Сервис для работы с проводками из 1С
  *
  * @author Alexandr Stegnin
  */
-
 @Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class InvestorCashService {
-
+  
+  private static final String SUCCESSFUL_SAVED = "Данные успешно сохранены";
   private static final LocalDate FILTERED_DATE = LocalDate.of(2020, 6, 30);
+
   MoneyRepository moneyRepository;
   InvestorService investorService;
   FacilityService facilityService;
@@ -59,8 +54,9 @@ public class InvestorCashService {
   TransactionLogService transactionLogService;
   UnderFacilityService underFacilityService;
   AccountTransactionService accountTransactionService;
-  AppUserRepository appUserRepository;
-  AccountRepository accountRepository;
+  ResaleShareService resaleShareService;
+  CashingService cashingService;
+  MoneyMapper moneyMapper;
 
   public ApiSuccessResponse update(InvestorCashDTO dto) {
     if (isBeforeFilteredDate(dto)) {
@@ -70,21 +66,21 @@ public class InvestorCashService {
     List<Money> monies = getByTransactionUUID(dto);
     if (isMoneyToDelete(dto, code, monies)) {
       monies.forEach(this::delete);
-      return new ApiSuccessResponse(HttpStatus.OK, "Проводки успешно удалены");
+      return new ApiSuccessResponse(HttpStatus.OK, SUCCESSFUL_SAVED);
     }
     if (AccountingCode.isResale(code)) {
-      resaleShare(dto);
-      return new ApiSuccessResponse(HttpStatus.OK, "Доля успешно перепродана");
+      resaleShareService.resaleShare(dto);
+      return new ApiSuccessResponse(HttpStatus.OK, SUCCESSFUL_SAVED);
     }
     if (AccountingCode.isCashing(code)) {
-      cashing(dto);
-      return new ApiSuccessResponse(HttpStatus.OK, "Деньги успешно выведены");
+      cashingService.cashing(dto);
+      return new ApiSuccessResponse(HttpStatus.OK, SUCCESSFUL_SAVED);
     }
     Money money = monies.isEmpty() ? null : monies.get(0);
     if (Objects.nonNull(money)) {
       transactionLogService.update(money);
       update(money, dto);
-      return new ApiSuccessResponse(HttpStatus.OK, "Данные успешно обновлены");
+      return new ApiSuccessResponse(HttpStatus.OK, SUCCESSFUL_SAVED);
     }
     money = moneyRepository.findMoney(dto.getDateGiven(), dto.getGivenCash(), dto.getFacility(),
         dto.getCashSource(), Constant.INVESTOR_PREFIX.concat(dto.getInvestorCode()));
@@ -97,280 +93,11 @@ public class InvestorCashService {
       update(money, dto);
     }
     log.info("Проводка успешно обновлена [{}]", dto);
-    return new ApiSuccessResponse(HttpStatus.OK, "Данные успешно сохранены");
+    return new ApiSuccessResponse(HttpStatus.OK, SUCCESSFUL_SAVED);
   }
 
   private boolean isMoneyToDelete(InvestorCashDTO dto, AccountingCode code, List<Money> monies) {
     return dto.isDelete() && !monies.isEmpty() && Objects.isNull(code);
-  }
-
-  /**
-   * Вывести деньги по данным из 1С
-   *
-   * @param dto DTO из 1С
-   */
-  private void cashing(InvestorCashDTO dto) {
-    if (dto.isDelete()) {
-      AccountTransaction parent = accountTransactionService.findByTransactionUUID(dto.getTransactionUUID());
-      Set<AccountTransaction> child = parent.getChild();
-      child.forEach(accountTransactionService::delete);
-      accountTransactionService.delete(parent);
-    } else {
-      AccountTransaction accountTransaction = accountTransactionService.findByTransactionUUID(dto.getTransactionUUID());
-      if (Objects.isNull(accountTransaction)) {
-        createCashingTransaction(dto);
-      } else {
-        updateCashingTransaction(accountTransaction, dto);
-      }
-    }
-  }
-
-  /**
-   * Провести перепродажу долю по данным из 1С
-   *
-   * @param dto DTO для перепродажи
-   */
-  private void resaleShare(InvestorCashDTO dto) {
-    if (dto.isDelete()) {
-      deleteResale(dto);
-    } else {
-      List<Money> monies = getByTransactionUUID(dto);
-      if (!monies.isEmpty()) {
-        throw new ApiException("Обновление проводки перепродажи доли не предусмотрено", HttpStatus.BAD_REQUEST);
-      } else {
-        createResaleShare(dto);
-      }
-    }
-  }
-
-  /**
-   * Создать проводку по перепродаже доли
-   *
-   * @param dto DTO суммы для перепродажи
-   */
-  private void createResaleShare(InvestorCashDTO dto) {
-    String investorSellerCode = dto.getInvestorSellerCode();
-    if (Objects.isNull(investorSellerCode)) {
-      throw new ApiException("Не указан код инвестора продавца", HttpStatus.PRECONDITION_FAILED);
-    }
-    String sellerLogin = INVESTOR_PREFIX.concat(investorSellerCode);
-    String buyerLogin = INVESTOR_PREFIX.concat(dto.getInvestorCode());
-    AppUser buyer = appUserRepository.findByLogin(buyerLogin);
-    if (Objects.isNull(buyer)) {
-      throw new ApiException("Не найден инвестор покупатель.", HttpStatus.NOT_FOUND);
-    }
-    String facility = dto.getFacility();
-    List<Money> sellerMonies = moneyRepository.getMoniesByInvestorAndFacility(sellerLogin, facility);
-
-    BigDecimal buyerSum = dto.getGivenCash();
-    checkSellerMonies(sellerMonies, buyerSum);
-
-    resaleShare(sellerMonies, buyer, dto);
-  }
-
-  private void checkSellerMonies(List<Money> sellerMonies, BigDecimal buyerSum) {
-    BigDecimal sellerSum = sellerMonies.stream()
-        .map(Money::getGivenCash)
-        .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-    if (sellerMonies.isEmpty() || sellerSum.compareTo(buyerSum) < 0) {
-      String message = String.format("Недостаточно денег для перепродажи. Сумма продавца = %s", sellerSum.longValue());
-      log.error(message);
-      throw new ApiException(message, HttpStatus.PRECONDITION_FAILED);
-    }
-  }
-
-  private void resaleShare(List<Money> sellerMonies, AppUser buyer, InvestorCashDTO dto) {
-    BigDecimal buyerSum = dto.getGivenCash();
-    Money sellerEqualSum = sellerMonies.stream()
-        .filter(money -> money.getGivenCash().compareTo(buyerSum) == 0)
-        .findAny()
-        .orElse(null);
-
-    if (Objects.isNull(sellerEqualSum)) {
-      BigDecimal sellerSum = sellerMonies.stream()
-          .map(Money::getGivenCash)
-          .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-      if (isBuyerSumAroundSellerSum(buyerSum, sellerSum)) {
-        for (Money sellerMoney : sellerMonies) {
-          closeSellerAndOpenBuyerMonies(sellerMoney, buyer, dto);
-        }
-      } else if (isBuyerSumLowerThanSellerSum(buyerSum, sellerSum)) {
-        Money sellerBiggestSum = sellerMonies.stream().max(Comparator.comparing(Money::getGivenCash))
-            .orElseThrow();
-        divideMonies(sellerBiggestSum, buyerSum);
-        closeSellerAndOpenBuyerMonies(sellerBiggestSum, buyer, dto);
-      }
-    } else {
-      closeSellerAndOpenBuyerMonies(sellerEqualSum, buyer, dto);
-    }
-  }
-
-  private void closeSellerAndOpenBuyerMonies(Money sellerSum, AppUser buyer, InvestorCashDTO dto) {
-    Investor investor = investorService.findByLogin(buyer.getLogin());
-    CashSource cashSource = findCashSource(dto.getCashSource());
-
-    Money buyMoney = new Money(sellerSum, investor, 4L, dto.getDateGiven(),
-        dto.getTransactionUUID(), cashSource);
-
-    String source = sellerSum.getId().toString();
-    buyMoney.setSource(source);
-    createResaleTransaction(dto, buyMoney, buyer);
-    sellerSum.setTypeClosingId(9L);
-    sellerSum.setDateClosing(dto.getDateGiven());
-    moneyRepository.save(sellerSum);
-  }
-
-  private boolean isBuyerSumLowerThanSellerSum(BigDecimal buyerSum, BigDecimal sellerSum) {
-    return buyerSum.compareTo(sellerSum) < 0;
-  }
-
-  private boolean isBuyerSumAroundSellerSum(BigDecimal buyerSum, BigDecimal sellerSum) {
-    BigDecimal inaccuracy = BigDecimal.valueOf(0.5);
-    return buyerSum.compareTo(sellerSum.subtract(inaccuracy)) >= 0 &&
-        buyerSum.compareTo(sellerSum.add(inaccuracy)) <= 0;
-  }
-
-  private void divideMonies(Money sellerBiggestSum, BigDecimal buyerSum) {
-    BigDecimal newSellerSum = sellerBiggestSum.getGivenCash().subtract(buyerSum);
-    Money newSellerMoney = new Money(sellerBiggestSum);
-    newSellerMoney.setGivenCash(newSellerSum);
-    newSellerMoney.setSource(sellerBiggestSum.getId().toString());
-    sellerBiggestSum.setGivenCash(buyerSum);
-    moneyRepository.save(sellerBiggestSum);
-    moneyRepository.save(newSellerMoney);
-  }
-
-  /**
-   * Создать транзакции по счетам клиентов
-   *
-   * @param dto      DTO суммы для перепродажи
-   * @param buyMoney сумма у инвестора покупателя
-   * @param buyer    инвестор покупатель
-   */
-  private void createResaleTransaction(InvestorCashDTO dto, Money buyMoney, AppUser buyer) {
-    Account owner = accountRepository.findByOwnerIdAndOwnerType(buyer.getId(), OwnerType.INVESTOR);
-    if (Objects.isNull(owner)) {
-      throw new ApiException("Не найден счёт инвестора покупателя", HttpStatus.NOT_FOUND);
-    }
-    AccountTransaction debitTx = accountTransactionService.createInvestorDebitTransaction(owner, buyMoney,
-        CashType.RE_BUY_SHARE, dto.getAccountingCode());
-    accountTransactionService.createCreditTransaction(owner, buyMoney, debitTx, CashType.RE_BUY_SHARE);
-  }
-
-  /**
-   * Удалить проводку перепродажи, пришедшую через 1С
-   *
-   * @param dto DTO для удаления
-   */
-  private void deleteResale(InvestorCashDTO dto) {
-    List<Money> monies = getByTransactionUUID(dto);
-    if (monies.isEmpty()) {
-      throw new ApiException("Не найдена сумма для удаления", HttpStatus.NOT_FOUND);
-    }
-    for (Money money : monies) {
-      deleteAccountTransactions(money);
-      Long sourceMoneyId = releaseRelatedMonies(money);
-      releaseMoniesBySource(money);
-      moneyRepository.delete(money);
-      releaseParentAndDeleteSourceMoney(sourceMoneyId);
-    }
-  }
-
-  private void releaseParentAndDeleteSourceMoney(Long sourceMoneyId) {
-    if (Objects.nonNull(sourceMoneyId)) {
-      List<Money> sourceMonies = moneyRepository.findBySource(sourceMoneyId.toString());
-      if (sourceMonies.size() == 1) {
-        Money sourceMoney = sourceMonies.get(0);
-        if (Objects.nonNull(sourceMoney.getSourceMoneyId())) {
-          Money parentMoney = moneyRepository.findById(sourceMonies.get(0).getSourceMoneyId()).orElse(null);
-          if (Objects.nonNull(parentMoney)) {
-            parentMoney.setGivenCash(parentMoney.getGivenCash().add(sourceMoney.getGivenCash()));
-            moneyRepository.save(parentMoney);
-            moneyRepository.delete(sourceMoney);
-          }
-        }
-      }
-    }
-  }
-
-  @Nullable
-  private Long releaseRelatedMonies(Money money) {
-    Long sourceMoneyId = money.getSourceMoneyId();
-    if (Objects.nonNull(sourceMoneyId)) {
-      Money relatedMoney = moneyRepository.findById(sourceMoneyId).orElse(null);
-      if (Objects.nonNull(relatedMoney)) {
-        relatedMoney.setTypeClosingId(null);
-        relatedMoney.setDateClosing(null);
-        moneyRepository.save(relatedMoney);
-      }
-    }
-    return sourceMoneyId;
-  }
-
-  private void releaseMoniesBySource(Money money) {
-    String source = money.getSource();
-    if (Objects.nonNull(source)) {
-      List<Long> ids = Arrays.stream(source.split("\\|"))
-          .map(Long::valueOf)
-          .collect(Collectors.toList());
-      ids.forEach(id -> {
-        Money m = moneyRepository.findById(id).orElse(null);
-        if (Objects.nonNull(m)) {
-          m.setTypeClosingId(null);
-          m.setDateClosing(null);
-          moneyRepository.save(m);
-        }
-      });
-    }
-  }
-
-  private void deleteAccountTransactions(Money money) {
-    AccountTransaction transaction = money.getTransaction();
-    if (Objects.isNull(transaction)) {
-      throw new ApiException("Не найдена транзакция по перепродаже доли", HttpStatus.NOT_FOUND);
-    }
-    AccountTransaction parentTx = transaction.getParent();
-    accountTransactionService.delete(transaction);
-    if (Objects.nonNull(parentTx)) {
-      accountTransactionService.delete(parentTx);
-    }
-  }
-
-  /**
-   * Обновить транзакцию по выводу средств
-   *
-   * @param accountTransaction транзакция по счёту
-   * @param dto                DTO из 1С
-   */
-  private void updateCashingTransaction(AccountTransaction accountTransaction, InvestorCashDTO dto) {
-    BigDecimal cash = dto.getGivenCash();
-    transactionLogService.update(accountTransaction);
-    prepareAccountTransaction(accountTransaction, dto);
-    AccountingCode accountingCode = AccountingCode.fromCode(dto.getAccountingCode());
-    Set<AccountTransaction> child = accountTransaction.getChild();
-    child.forEach(c -> {
-      c.setCash(cash.multiply(COMMISSION_RATE));
-      c.setAccountingCode(accountingCode.getCode());
-      accountTransactionService.update(c);
-    });
-    accountTransactionService.update(accountTransaction);
-  }
-
-  /**
-   * Создать транзакцию по выводу средств
-   *
-   * @param dto DTO из 1C для вывода
-   */
-  private void createCashingTransaction(InvestorCashDTO dto) {
-    Money money = convert(dto);
-    money.setDateClosing(dto.getDateGiven());
-    AccountingCode accountingCode = AccountingCode.fromCode(dto.getAccountingCode());
-    AccountTransaction accountTransaction = accountTransactionService.cashing(money, accountingCode);
-    if (Objects.nonNull(accountTransaction)) {
-      transactionLogService.cashing(accountTransaction);
-    }
   }
 
   /**
@@ -392,7 +119,7 @@ public class InvestorCashService {
    * @return - созданная сумма
    */
   private Money create(InvestorCashDTO dto) {
-    Money money = convert(dto);
+    Money money = moneyMapper.convert(dto);
     moneyRepository.save(money);
     transferMoney(money);
     return money;
@@ -458,52 +185,6 @@ public class InvestorCashService {
       Investor investor = findInvestor(dto.getInvestorCode());
       money.setInvestor(investor);
     }
-  }
-
-  /**
-   * Поготовить к обновлению транзакцию
-   *
-   * @param accountTransaction существующая транзакция по счёту
-   * @param dto                DTO объект из 1С
-   */
-  private void prepareAccountTransaction(AccountTransaction accountTransaction, InvestorCashDTO dto) {
-    accountTransaction.setCash(dto.getGivenCash());
-    accountTransaction.setTxDate(DateUtils.convert(dto.getDateGiven()));
-    AccountingCode accountingCode = AccountingCode.fromCode(dto.getAccountingCode());
-    accountTransaction.setAccountingCode(accountingCode.getCode());
-    if (!accountTransaction.getOwner().getOwnerName().equalsIgnoreCase(INVESTOR_PREFIX.concat(dto.getInvestorCode()))) {
-      AppUser investor = appUserRepository.findByLogin(INVESTOR_PREFIX.concat(dto.getInvestorCode()));
-      Account account = accountRepository.findByOwnerIdAndOwnerType(investor.getId(), OwnerType.INVESTOR);
-      accountTransaction.setOwner(account);
-    }
-  }
-
-  /**
-   * Конвертировать DTO в проводку
-   *
-   * @param dto - DTO объект из 1С
-   * @return - подготовленная к сохранению проводка
-   */
-  private Money convert(InvestorCashDTO dto) {
-    Money money = new Money();
-
-    Investor investor = findInvestor(dto.getInvestorCode());
-    money.setInvestor(investor);
-
-    Facility facility = findFacility(dto.getFacility());
-    money.setFacility(facility);
-    UnderFacility underFacility = findUnderFacility(facility);
-    money.setUnderFacility(underFacility);
-
-    money.setShareType(ShareType.MAIN);
-
-    CashSource cashSource = findCashSource(dto.getCashSource());
-    money.setCashSource(cashSource);
-
-    money.setDateGiven(dto.getDateGiven());
-    money.setGivenCash(dto.getGivenCash());
-    money.setTransactionUUID(dto.getTransactionUUID());
-    return money;
   }
 
   /**
